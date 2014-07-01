7b82e63333bf6885933a4efd818bd1d40aed98f3
johannes k <johanneskneip@web.de>
2014-07-02 00:54:07 +0200

    deleted json scaffold stuff

--- app/controllers/boulders_controller.rb
+++ app/controllers/boulders_controller.rb
@@ -2,13 +2,11 @@ class BouldersController < ApplicationController
   before_action :set_boulder, only: [:show, :edit, :update, :destroy]
 
   # GET /boulders
-  # GET /boulders.json
   def index
     @boulders = Boulder.all
   end
 
   # GET /boulders/1
-  # GET /boulders/1.json
   def show
   end
 
@@ -22,23 +20,19 @@ class BouldersController < ApplicationController
   end
 
   # POST /boulders
-  # POST /boulders.json
   def create
     @boulder = Boulder.new(boulder_params)
 
     respond_to do |format|
       if @boulder.save
         format.html { redirect_to @boulder, notice: 'Boulder was successfully created.' }
-        format.json { render :show, status: :created, location: @boulder }
       else
         format.html { render :new }
-        format.json { render json: @boulder.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /boulders/1
-  # PATCH/PUT /boulders/1.json
   def update
     respond_to do |format|
       if @boulder.update(boulder_params)
@@ -52,12 +46,10 @@ class BouldersController < ApplicationController
   end
 
   # DELETE /boulders/1
-  # DELETE /boulders/1.json
   def destroy
     @boulder.destroy
     respond_to do |format|
       format.html { redirect_to boulders_url, notice: 'Boulder was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
