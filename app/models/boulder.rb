ef6684bcca920a2a447423b32d0ab29baca042dc
PyDjango <johanneskneip@web.de>
2014-07-02 20:12:44 +0200

    added a grade model, cleaned scaffolding, added relations

--- app/models/boulder.rb
+++ app/models/boulder.rb
@@ -1,4 +1,5 @@
 class Boulder < ActiveRecord::Base
 	validates :name, presence: true, length: { in: 2..45 }
 	belongs_to :block
+	has_one :grade
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140702150546) do
+ActiveRecord::Schema.define(version: 20140702181034) do
 
   create_table "blocks", force: true do |t|
     t.string   "name"
@@ -30,6 +30,12 @@ ActiveRecord::Schema.define(version: 20140702150546) do
   end
 
   add_index "boulders", ["block_id"], name: "index_boulders_on_block_id"
+
+  create_table "grades", force: true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "regions", force: true do |t|
     t.string   "name"
